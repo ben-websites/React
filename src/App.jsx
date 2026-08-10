db9a913c70470886e435b97e802d98a1243db34c
ben-websites <sabihuddin309@gmail.com>
2026-08-10 05:10:47 +0500

    Remove unnecessary blank line in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-
 import { CartProvider } from "./contextApi/CartContext";
 import { ProductProvider } from "./contextApi/ProductContext";
 
